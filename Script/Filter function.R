850e417c76a19854a1c8f2cd9deab329163856a9
Oluwafemi OYEDELE <oluwafemioyedele908@gmail.com>
2022-03-17 17:13:35 +0100

    Modified my sum function

--- Script/Filter function.R
+++ Script/Filter function.R
@@ -18,3 +18,28 @@ library(tidyverse)
  msleep %>%
    filter(across(.cols = everything(),.fns = ~!is.na(.x)))
 
+
+
+ # Rowwise function review
+ # Sum to first NA
+ ID <- c("A", "B", "C", "D", "E")
+ Q1 <- c(NA, 1, 2, NA, 4)
+ Q2 <- c(5, NA, 7, 8, 9)
+ Q3 <- c(0, 1, NA, 3, 4)
+ df <- data.frame(ID, Q1, Q2, Q3)
+
+ sum_to_first_na <- function(x) {
+   ss <- 0
+   i <- 1
+   repeat {
+     if (is.na(x[i])) break
+     ss <- ss + x[i]
+     i <- i + 1
+   }
+   return(ss)
+ }
+
+ df$sum <- apply(df[, 2:4], 1, sum_to_first_na)
+
+  df
+
