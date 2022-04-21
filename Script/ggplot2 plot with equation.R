849122c548d8dd5f5be866de6c9748e669e71fb6
Oluwafemi OYEDELE <oluwafemioyedele908@gmail.com>
2022-04-21 17:03:16 +0100

    added another approach for my reg equation

--- Script/ggplot2 plot with equation.R
+++ Script/ggplot2 plot with equation.R
@@ -51,5 +51,56 @@ p <- ggplot(data = df, aes(x = x, y = y)) +
                parse = TRUE) +
   geom_point()
 
+
 p
+
+
+
+# Another Approach
+
+library(ggpmisc)
+df <- data.frame(x = c(1:100))
+df$y <- 20 * c(0, 1) + 3 * df$x + rnorm(100, sd = 40)
+df$group <- factor(rep(c("A", "B"), 50))
+
+my.formula <- y ~ x
+
+p <- ggplot(data = df, aes(x = x, y = y, colour = group)) +
+geom_smooth(method = "lm", se=FALSE, formula = my.formula) +
+stat_poly_eq(formula = my.formula,
+aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")),
+parse = TRUE) +
+geom_point()
+
+p
+
+
+
+p <- ggplot(data = df, aes(x = x, y = y, linetype = group)) +
+geom_smooth(method = "lm", se=FALSE, formula = my.formula) +
+stat_poly_eq(formula = my.formula,
+aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")),
+parse = TRUE) +
+geom_point()
+
+p
+
+
+
+
+
+# GGPURBR Option
+
+library(ggpubr)
+
+# reproducible data
+set.seed(1)
+df <- data.frame(x = c(1:100))
+df$y <- 2 + 3 * df$x + rnorm(100, sd = 40)
+
+# By default showing Pearson R
+ggscatter(df, x = "x", y = "y", add = "reg.line") +
+  stat_cor(label.y = 300) +
+  stat_regline_equation(label.y = 280)
+
 
