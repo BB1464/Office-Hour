1d81bc1b7d228415f0aab33684457a8ead001b0d
Oluwafemi OYEDELE <oluwafemioyedele908@gmail.com>
2022-04-21 15:43:09 +0100

    added the regression line equation to my visualization

---
+++ Script/ggplot2 plot with equation.R
@@ -0,0 +1,55 @@
+
+# Initial problem
+
+library(ggplot2)
+
+df <- data.frame(x = c(1:100))
+df$y <- 2 + 3 * df$x + rnorm(100, sd = 40)
+
+p <- ggplot(data = df, aes(x = x, y = y)) +
+  geom_smooth(method = "lm", se=FALSE, color="black", formula = y ~ x) +
+  geom_point()
+
+p
+
+
+
+# Add Significant Asterisk to the plot
+
+lm_eqn <- function(df){
+
+m <- lm(y ~ x, df);
+  eq <- substitute(italic(y) == a + b %.% italic(x)*","~~italic(r)^2~"="~r2,
+                   list(a = format(unname(coef(m)[1]), digits = 2),
+                        b = format(unname(coef(m)[2]), digits = 2),
+                        r2 = format(summary(m)$r.squared, digits = 3)))
+  as.character(as.expression(eq));
+}
+
+p1 <- p + geom_text(x = 25, y = 300, label = lm_eqn(df), parse = TRUE)
+
+
+p1
+
+
+# Second Step
+
+
+library(ggplot2)
+library(ggpmisc)
+
+df <- data.frame(x = c(1:100))
+df$y <- 2 + 3 * df$x + rnorm(100, sd = 40)
+
+
+my.formula <- y ~ x
+
+p <- ggplot(data = df, aes(x = x, y = y)) +
+  geom_smooth(method = "lm", se=FALSE, color="black", formula = my.formula) +
+  stat_poly_eq(formula = my.formula,
+               aes(label = paste(..eq.label.., ..rr.label.., sep = "~~~")),
+               parse = TRUE) +
+  geom_point()
+
+p
+
