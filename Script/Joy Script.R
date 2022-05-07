9580d427c1dc55b4384ae309489983ee3585181b
Oluwafemi OYEDELE <oluwafemioyedele908@gmail.com>
2022-05-07 12:42:12 +0100

    added ethno plot

--- Script/Joy Script.R
+++ Script/Joy Script.R
@@ -80,3 +80,79 @@ ggplot(data = data,mapping = aes(Community,`popularity among farmers and the peo
 
 # Save the graph
 ggsave(path = here::here('Plot'),width = 10,height = 8,dpi = 400,'Ethnobotany1.png')
+
+
+
+
+# Ethnobotany
+data <- read_excel('Data/Copy of Questionnaire_Market.xlsx',sheet = 'Ethnobotany and Uses')
+
+data$`Frequency of cooking` <- factor(data$`Frequency of cooking`)
+data$`Main cooking method` <- factor(data$`Main cooking method`)
+data$`Uses of the plant` <- factor(data$`Uses of the plant`)
+
+data1 <- data %>% mutate(`Main cooking method`=fct_recode(`Main cooking method`,'Boiling'='1','Local Specialties'='4')) %>% mutate(`Frequency of cooking`=fct_recode(`Frequency of cooking`,'Weekly'='2','Occasional'='3')) %>%  mutate(`Uses of the plant`=fct_recode(`Uses of the plant`,'Food'='1','Food and Animal Feed'='2','Food and Export Commodity'='3'))
+
+
+
+
+ggplot(data = data1,aes(x = State,fill=`Uses of the plant`))+
+  geom_bar()+
+  facet_grid(`Main cooking method`~`Frequency of cooking`)+
+  theme_test()+scale_fill_brewer(palette = 'Set1')+
+  theme(strip.background = element_rect(fill = NA),axis.text.x = element_text(angle = 45,hjust = 1))
+
+
+ggsave(path=here::here('Plot'),'Ethno2.png',width = 10,height = 7,dpi = 400)
+
+
+
+# Ethno 3
+data$`History of Plant use` <- factor(data$`History of Plant use`)
+
+
+data1 <- data %>%
+  mutate(`History of Plant use`=fct_recode(`History of Plant use`,'Ancestral/Indigenous'='1','Introduced_Unknown Distant Past'='2','Introduced_Time of Introduction Known'='3'))
+
+
+ggplot(data=data1,aes(x = Community,fill=`History of Plant use`))+geom_bar(position = position_dodge())+
+  theme_test()+
+  scale_fill_brewer(palette = 'Set1')+
+  theme(axis.text.x = element_text(hjust = 1,angle = 45))
+
+
+ggsave(path=here::here('Plot'),'Ethno3.png',width = 10,height = 7,dpi = 400)
+
+
+# Taro Production and Contraint
+
+data <- read_excel('Data/Copy of Questionnaire_Market.xlsx',sheet = 'Taro pdn n constraints')
+
+
+# Taro Constraint
+data$`Affected by TLB` <- factor(data$`Affected by TLB`)
+data$`Taro Production Trend` <- factor(data$`Taro Production Trend`)
+
+
+data1 <- data %>% mutate(`Affected by TLB`=fct_recode(`Affected by TLB`,'Presence of TLB'='1','Absence of TLB'='2')) %>%
+  mutate(`Taro Production Trend`=fct_recode(`Taro Production Trend`,'Decreasing'='1','About the Same'='2','Increasing'='3'))
+
+
+
+ggplot(data1,aes(x=`Years of growing taro`,fill=`Taro Production Trend`))+geom_bar()+facet_grid(~`Affected by TLB`)+
+  theme(strip.background.x = element_rect(fill = 'white',colour = 'black'))+
+  theme_test()+
+scale_fill_brewer(palette = 'Set1')
+
+
+ggsave(path=here::here('Plot'),'Production1.png',width = 10,height = 7,dpi = 400)
+
+
+# Production Trend
+
+ggplot(data,aes(x = Year,y = Production,group=1))+stat_summary(geom = 'line',size=1)+labs(y='Taro Production (kg/yr)')+theme_classic()
+
+ggsave(path=here::here('Plot'),'Production2.png',width = 10,height = 7,dpi = 400)
+
+
+
