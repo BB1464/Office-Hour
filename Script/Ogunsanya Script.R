cb9b1cd9ddee72d65deb96a841602cda040793e6
Oluwafemi OYEDELE <oluwafemioyedele908@gmail.com>
2022-05-13 13:03:01 +0100

    add new files

--- Script/Ogunsanya Script.R
+++ Script/Ogunsanya Script.R
@@ -14,33 +14,84 @@ library(cowplot)
 data1 <- read_excel('Data/Copy of Field data Mature trait.xlsx')
 
 
-data1 <- data1 %>% mutate(across(.cols = c(1:4),.fns = factor))
+data1 <- data1 %>% mutate(across(.cols = c(1:2),.fns = factor))
 
 
 # Stem Colour
-p1 <- ggplot(data = data1,mapping = aes(x = Species,y = `Stem Colour`,fill=Category...4))+geom_col()
+p1 <- ggplot(data = data1,mapping = aes(x = Species,y = `Stem Colour`,fill=Category...6))+geom_col()+scale_fill_brewer(name='',palette = 'Set1')+
+  theme(axis.title.y = element_text(family = 'serif',size = 10))
 
 
 # Leaf Margin colours
-p2 <- ggplot(data = data1,mapping = aes(x = Species,y = `Leaf Margin Colour`,fill=Category...8))+geom_col()
+p2 <- ggplot(data = data1,mapping = aes(x = Species,y = `Leaf Margin Colour`,fill=Category...8))+geom_col()+
+  scale_fill_brewer(name='',palette = 'Set1')+
+  theme(axis.title.y = element_text(family = 'serif',size = 10))
 
 
-#Petiole Colour
-ggplot(data = data1,aes(x = Species,y = `Petiole Colour`,fill=Category...8))+
-  geom_col()
+# Petiole Colour
+p3 <- ggplot(data = data1,aes(x = Species,y = `Petiole Colour`,fill=Category...10))+
+  geom_col()+
+  scale_fill_brewer(name='',palette = 'Set1')+
+  theme(axis.title.y = element_text(family = 'serif',size = 10))
 
 
 # Leaf density
-ggplot(data = data1,aes(x = Species,y = `Leaf density`,fill=Category...12))+
-  geom_col()
+p4 <- ggplot(data = data1,aes(x = Species,y = `Leaf density`,fill=Category...12))+
+  geom_col()+
+  scale_fill_brewer(name='',palette = 'Set1')+
+  theme(axis.title.y = element_text(family = 'serif',size = 10))
 
 # Leaf Colour
-ggplot(data = data1,aes(x = Species,y = `Leaf Colour-`,fill=Category...14))+
-  geom_col()
+p5 <- ggplot(data = data1,aes(x = Species,y = `Leaf Colour-`,fill=Category...14))+
+  geom_col()+
+  scale_fill_brewer(name='',palette = 'Set1')+
+  theme(axis.title.y = element_text(family = 'serif',size = 10))
+
+# Leaf Vein Colour Upper
+p6 <- ggplot(data = data1,aes(x = Species,y = `Leaf vein colour(Upper Surface)`,fill=Category...16))+
+  geom_col()+
+  scale_fill_brewer(name='',palette = 'Set1')+
+  theme(axis.title.y = element_text(family = 'serif',size = 10))
+
+
+# Leaf Vein Colour (Lower Surface)
+p7 <- ggplot(data = data1,aes(x = Species,y = `Leaf vein colour(Lower Surface)`,fill=Category...18))+
+  geom_col()+
+  scale_fill_brewer(name='',palette = 'Set1')+
+  theme(axis.title.y = element_text(family = 'serif',size = 10))
+
+
+
+# Leaf Shape
+p8 <- ggplot(data = data1,aes(x = Species,y = `Leaf Shape`,fill=Category...20))+
+  geom_col()+
+  scale_fill_brewer(name='',palette = 'Set1')
+
+# Leaf Apex Shape
+p9 <- ggplot(data = data1,aes(x = Species,y = `Leaf apex shape`,fill=Category...22))+
+  geom_col()+
+  scale_fill_brewer(name='',palette = 'Set1')+
+  theme(axis.title.y = element_text(family = 'serif',size = 10))
+
+# Distance between lobes
+p10 <- ggplot(data = data1,aes(x = Species,y = `Distance between lobes`,fill=Category...24))+
+  geom_col()+
+  scale_fill_brewer(name='',palette = 'Set1')+
+  theme(axis.title.y = element_text(family = 'serif',size = 10))
+
+# Vigour
+p11 <- ggplot(data = data1,mapping = aes(x = Species,y = Vigour,fill=Category...4))+geom_col()+scale_fill_brewer(name='',palette = 'Set1')+
+  theme(axis.title.y = element_text(family = 'serif',size = 10))
+
+
+plot_grid(p6,p2,p3,p4,p5,p1,p7,p8,p9,p10,nrow = 4,ncol = 4,rel_widths = c(1,1),labels = 'AUTO',label_size = 8,label_fontfamily = 'serif')
+
+
+ggsave(path = here::here('Plot'),'Demo.png',width = 17,height = 12,dpi = 450)
 
 
 
 
+# Analysis for Pot Experiment
 
-
-plot_grid(p1,p2,nrow = 2,ncol = 1,rel_widths = c(1,1))
+data1 <- read_excel('Data/Copy of Mature trait data pot exp.xlsx')
