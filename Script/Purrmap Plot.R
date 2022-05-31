0d138c817d9bbc8c6748488ba61c4c3d450af309
Oluwafemi OYEDELE <oluwafemioyedele908@gmail.com>
2022-05-31 10:04:55 +0100

    add ttest function

--- Script/Purrmap Plot.R
+++ Script/Purrmap Plot.R
@@ -22,3 +22,77 @@ Plot |> pull()
 
 # Save the Plot
 map2(paste0(Plot$plot, ".pdf"), Plot$plot, ggsave)
+
+
+# Model
+mtcars_nested <- mtcars %>%
+  group_by(cyl) %>%
+  nest()
+
+
+mtcars_nested
+
+# Fit the Model
+mtcars_nested <- mtcars_nested %>%
+  mutate(model = map(data, function(df) ~lm(mpg ~ wt, data = df)))
+
+mtcars_nested
+
+
+
+# List of Prediction
+
+mtcars_nested <- mtcars_nested %>%
+  mutate(model = map(model, predict))
+
+mtcars_nested
+
+
+
+library(purrr)
+
+mtcars %>%
+  split(.$cyl) %>% # from base R
+  map(~ lm(mpg ~ wt, data = .)) %>%
+  map(summary) %>%
+  map_dbl("r.squared")
+
+# purr model function
+
+mtcars_model <- mtcars |> group_by(cyl) |>
+  nest() |>
+  mutate(model=map(.x = data,.f = ~lm(formula = mpg~vs+am,data = .))) |>
+mutate(model_summary=map(.x = model,.f = ~summary(.))) |>
+  mutate(anova=map(.x = model,.f = ~anova(.))) |>
+  mutate(rsqr=map_dbl(.x = model_summary,.f = ~.[['r.squared']]))
+
+
+# View the output from the model
+mtcars_model$anova[[1]]
+mtcars_model$model_summary[[1]]
+
+mtcars_model$model_summary[[1]][['r.squared']]
+
+
+mtcars_model |> pluck('model_summary',1)
+
+mtcars_model |> pluck('anova',1)
+
+
+mtcars_model |> pluck('anova')
+
+mtcars_model |> pluck('model_summary')
+
+mtcars_model |> pluck('model')
+
+mtcars_model |> chuck('anova')
+
+
+# Shot cut for anova and summary
+summary(mtcars_model$model[[1]]) # for Anova
+
+anova(mtcars_model$model[[1]]) # Return the summary
+
+
+
+
---
+++ Script/ttest_function.R
@@ -0,0 +1,50 @@
+dat <- structure(list(
+  group = structure(c(
+    1L, 1L, 1L, 1L, 1L, 1L, 1L,
+    1L, 1L, 1L, 1L, 1L, 1L, 1L, 1L, 2L, 2L, 2L, 2L, 2L, 2L, 2L, 2L,
+    2L, 2L, 2L, 2L, 2L, 2L, 2L
+  ), .Label = c("a", "b"), class = "factor"),
+  resp = c(
+    10.48, 9.87, 11.1, 8.56, 11.15, 9.53, 8.99, 10.06,
+    11.02, 10.57, 11.85, 10.11, 9.25, 11.66, 10.72, 8.34, 10.58,
+    10.47, 9.46, 11.13, 8.35, 9.69, 9.82, 11.47, 9.13, 11.53,
+    11.05, 11.03, 10.84, 10.22
+  ), slp = c(
+    38.27, 46.33, 44.29,
+    35.57, 34.78, 47.81, 50.45, 46.31, 47.82, 42.07, 31.75, 65.65,
+    47.42, 41.51, 38.69, 47.84, 46.22, 50.66, 50.69, 44.09, 47.3,
+    52.53, 53.63, 53.38, 27.34, 51.83, 56.63, 32.99, 77.5, 38.24
+  ), grad = c(
+    0.3, 0.66, 0.57, 0.23, 0.31, 0.48, 0.5, 0.49,
+    2.41, 0.6, 0.27, 0.89, 2.43, 1.02, 2.17, 1.38, 0.17, 0.47,
+    1.1, 3.28, 6.14, 3.8, 4.35, 0.85, 1.13, 1.11, 2.93, 1.13,
+    4.52, 0.13
+  )
+), class = "data.frame", row.names = c(NA, -30L))
+
+head(dat)
+
+
+ttest_fun = function(response) {
+  form = paste(response, "~ group")
+  lm(as.formula(form), data = dat)
+}
+
+
+
+ttest_fun(response = "resp")
+
+# Looping through the response variable
+vars = names(dat)[2:4]
+
+vars
+
+# List the function by names without using a formula
+
+models = vars %>%
+  map(ttest_fun)
+
+
+vars %>%
+  set_names() %>%
+  map(ttest_fun)
