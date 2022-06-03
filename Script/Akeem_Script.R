7168e15b20219320905988f1bfbbb4fe8a17c043
Oluwafemi OYEDELE <oluwafemioyedele908@gmail.com>
2022-06-03 13:13:23 +0100

    add akeem script

---
+++ Script/Akeem_Script.R
@@ -0,0 +1,24 @@
+library(tidyverse)
+library(readxl)
+
+
+dat <- read_excel(path = '~/../Desktop/dat.xlsx',skip = 1)
+
+# Temp and Relative Humidity
+tem <- dat |> filter(...1 %in%c('Ambient Temperature','Relative humidity'))
+
+
+# Colour of the Animal
+dat1 <- dat |> filter(...1!='Ambient Temperature') |>
+  filter(...1!='Relative humidity') |> pivot_longer(-1)
+
+# Join the two
+
+# Split the variable column
+dat3 <- dat1 |> separate(col = ...1,into = c('Colour','Rep'))
+
+
+mod <- lm(value~Rep+Colour+name,data=dat3)
+anova(mod)
+
+HSD.test(y = mod,trt = name,console = TRUE)
