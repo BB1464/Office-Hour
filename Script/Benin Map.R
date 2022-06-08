462eb7892ada4adf04d84fe2006a92229d8eba33
Oluwafemi OYEDELE <oluwafemioyedele908@gmail.com>
2022-06-08 15:22:43 +0100

    updated my plot

---
+++ Script/Benin Map.R
@@ -0,0 +1,26 @@
+
+
+library(tidyverse)
+library(sf)
+library(afrilearndata)
+library(afriadmin)
+library(rnaturalearthdata)
+library(rnaturalearth)
+library(readxl)
+library(ggrepel)
+
+
+dat <- read_excel('Data/benin_natasha.xlsx')
+
+
+Benin <- ne_states(country = 'Benin')
+
+Benin <- st_as_sf(Benin)
+
+# Map of Benin
+ggplot(data = Benin,aes(x = longitude,y = latitude))+geom_sf()+
+  geom_point(data = dat,aes(x = Longitude,y = latitude,col=Village))+
+  scale_colour_manual(values = c("#006400","#8B2323","#00008B","#8B3E2F",'#567899',"#FF1493","#787878","#00EE00","#FF4500","#030303","#00EEEE","#8470FF","#CD0000","#CD0000","#FFFF00","#A2B5CD","#D15FEE"))+
+  geom_text_repel(data = Benin,aes(label=woe_name))+
+  labs(col = 'Sampling Site')+
+  theme_void()
