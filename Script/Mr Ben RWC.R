bb5ba72aad8873ea5f6cd0a3a245dbd3f60d9e26
Oluwafemi OYEDELE <oluwafemioyedele908@gmail.com>
2022-07-04 07:11:17 +0100

    add my updated file

--- Script/Mr Ben RWC.R
+++ Script/Mr Ben RWC.R
@@ -1,14 +1,11 @@
 
-
-
-library(tidyverse)
-library(ggpmisc)
-library(ggtext)
-
-
-ggplot(data = iris,mapping = aes(x = Sepal.Length,y = Sepal.Width))+
-  geom_smooth(se = FALSE,formula = y~x)+
-  stat_poly_eq(mapping = aes(label=paste(..eq.label..,..adj.rr.label..,..p.value.label..,sep='~~~')),parse = TRUE,coef.digits = 3,p.digits = 3,small.p = TRUE)+theme_light()
+############################################################################
+############################################################################
+###                                                                      ###
+###                            EDA FOR DR BEN                            ###
+###                                                                      ###
+############################################################################
+############################################################################
 
 
 
@@ -42,9 +39,6 @@ ggsave('RWC_Plant.png',path = here::here('Plot'),width = 12,height = 10,dpi = 45
 
 dat1 <- read_excel(path = '~/../Desktop/Mr Femi.xlsx',sheet = 3)
 
-dat <- dat |> rename(RWC=`%RWC`)
-
-dat$RWC <- as.numeric(dat$RWC)
 
 
 
@@ -62,3 +56,79 @@ dat1 |>
 
 
 ggsave('RWC_Soil.png',path = here::here('Plot'),width = 12,height = 10,dpi = 450)
+
+
+
+
+# Correct Graph for Dr Ben
+dat2 <- read_excel(path = '~/../Desktop/Mr Femi.xlsx',sheet = 1)
+
+# Start the geoms with Rainfall
+
+
+dat2 |> mutate(Month = factor(
+  Month,
+  levels = c(
+    'Sept.2019',
+    'Oct.2019',
+    'Nov.2019',
+    'Dec.2019',
+    'Jan.2020',
+    'Feb.2020',
+    'Mar.2020',
+    'Sept.2020',
+    'Oct.2020',
+    'Nov.2020',
+    'Dec.2020',
+    'Jan.2021',
+    'Feb.2021',
+    'Mar.2021'
+  )
+)) |>
+  ggplot(aes(x = Month,y = `Rain(mm)`,fill='4'))+
+  stat_summary(geom = 'col',fun = 'mean')+
+  stat_summary(aes(x = Month,y = `Windspeed (km/hr)`,group=2,col='1'),geom = 'line',fun = 'mean',size=1)+
+  stat_summary(aes(x = Month,y = `Temp (°c)`,group=2,col='2'),geom = 'line',fun = 'mean',size=1)+
+  stat_summary(aes(x = Month,y = `Rel Hum (%)`,col='3',group=2),geom = 'line',fun = 'mean',size=1)+
+  scale_color_manual(name=NULL,values = c('red','blue','green'),labels = c(
+    'Wind Speed (km/hr)',
+    'Temperature \u00B0C',
+    'Relative Humidity (%)'),breaks = c('1','2','3'),guide=guide_legend(override.aes = list(shape=15)))+
+  scale_fill_manual(name=NULL,values = c('gray50'),breaks = '4',labels='Rainfall (mm)',guide=guide_legend(direction = 'horizontal',override.aes = list(shape=15)))+scale_y_continuous(
+    sec.axis = sec_axis(trans = ~ ., name = 'Temperature \u00B0C , Relative Humidity (%) and Wind Speed (km/hr)'),
+    name = 'Rainfall (mm)'
+  ) + scale_x_discrete(
+    labels = c(
+      'Sept 2019',
+      'Oct 2019',
+      'Nov 2019',
+      'Dec 2019',
+      'Jan 2020',
+      'Feb 2020',
+      'Mar 2020',
+      'Sept 2020',
+      'Oct 2020',
+      'Nov 2020',
+      'Dec 2020',
+      'Jan 2021',
+      'Feb 2021',
+      'Mar 2021'
+    )
+  ) +coord_cartesian(expand = FALSE,ylim = c(0,250))+
+  theme_classic()+
+  theme(
+    text = element_text(family = 'serif', size = 15),
+    axis.line = element_line(size = 1),
+    axis.ticks = element_line(size = 1)
+  )
+
+
+
+
+# Save the weather graph
+ggsave('Mr_Ben_Weather.png',path = here::here('Plot'),width = 15,height = 10,dpi = 450)
+
+
+
+
+
