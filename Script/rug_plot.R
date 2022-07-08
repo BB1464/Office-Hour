5eb3613d077e926d5755bf3ac11fbd2b63fca038
Oluwafemi OYEDELE <oluwafemioyedele908@gmail.com>
2022-07-08 11:56:00 +0100

    updated my rug

--- Script/rug_plot.R
+++ Script/rug_plot.R
@@ -22,7 +22,8 @@ ggplot(data = temp,aes(x = t_diff,xend=t_diff,y=zon-0.25,yend=zon+0.25))+
   scale_color_gradient2(low = 'darkblue',mid = 'white',high = 'darkred',midpoint = 0,guide = 'none')+
   scale_y_continuous(breaks =1:8,labels = zonal)+
   scale_x_continuous(breaks = seq(-3,4,1),labels = seq(-3,4,1),limits = c(-3,4))+
-  labs(x='Temperature anomally (\u00B0 C)',y=NULL,title = 'Variation in Annaual Temperature Annomally (1880-2021)',subtitle = 'Bars for 2021 are Colored by the Size of Annomally')
+  labs(x='Temperature anomally (\u00B0 C)',y=NULL,title = 'Variation in Annaual Temperature Annomally (1880-2021)',subtitle = 'Bars for 2021 are Colored by the Size of Annomally')+coord_cartesian(expand = FALSE,clip = 'off')+
+  theme(plot.background = element_rect(colour = 'black'),panel.background = element_rect(fill='white'),axis.line = element_line(size=1),axis.ticks = element_blank(),plot.title.position  = 'plot')
 
 
 
