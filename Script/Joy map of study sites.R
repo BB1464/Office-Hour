0e135ab6a1ed82766bf96790793aed45ec34ec8a
Oluwafemi OYEDELE <oluwafemioyedele908@gmail.com>
2022-08-12 19:33:51 +0100

    add my map of the various zones script

--- Script/Joy map of study sites.R
+++ Script/Joy map of study sites.R
@@ -8,12 +8,25 @@
 ###########################################################################
 
 
+
 library(tidyverse)
 library(readxl)
 library(rnaturalearth)
+library(sf)
+
+# Import the Agromophological maps data
+
+agro <- read_excel(path = here::here('Data/Agromorphological map coordinates.xlsx'),sheet = 2)
+
+#col <- read_excel(path = here::here('Data/Agro-Joy.xlsx'))
+
+# Converrt it to sf object
+
+#col1 <- col |> st_as_sf(coords=c('longitude','latitude'),crs=4326)
+
 
 # Farms
-dat2=read_excel('Data/maps.xlsx',sheet = 'Farms')
+dat2=read_excel('Data/maps.xlsx',sheet ='Farms')
 
 
 
@@ -34,19 +47,81 @@ dat$gn_name <- str_replace(string =dat$gn_name,pattern = 'Federal Capital Territ
 
 
 
+# data Wrangling for the Agro-Ecological Zones
+
+dat <- dat |>
+  mutate(name2=name) |>
+  mutate(name=if_else(condition = name=='Kebbi',true = 'Humid Savannah',false = name)) |>
+  mutate(name=if_else(condition = name=='Kano',true = 'Humid Savannah',false = name)) |>
+  mutate(name=if_else(condition = name=='Sokoto',true = 'Humid Savannah',false = name)) |>
+  mutate(name=if_else(condition = name=='Zamfara',true = 'Humid Savannah',false = name)) |>
+  mutate(name=if_else(condition = name=='Katsina',true = 'Humid Savannah',false = name)) |>
+  mutate(name=if_else(condition = name=='Jigawa',true = 'Humid Savannah',false = name)) |>
+  mutate(name=if_else(condition = name=='Yobe',true = 'Humid Savannah',false = name)) |>
+  mutate(name=if_else(condition = name=='Borno',true = 'Humid Savannah',false = name)) |>
+  mutate(name=if_else(condition = name=='Kaduna',true = 'Northern Guinea Savanna',false = name)) |>
+  mutate(name=if_else(condition = name=='Bauchi',true = 'Northern Guinea Savanna',false = name)) |>
+mutate(name=if_else(condition = name=='Gombe',true = 'Northern Guinea Savanna',false = name)) |>
+mutate(name=if_else(condition = name=='Oyo',true = 'Derived Savanna',false = name)) |>
+  mutate(name=if_else(condition = name=='Ogun',true = 'Derived Savanna',false = name)) |>
+  mutate(name=if_else(condition = name=='Osun',true = 'Derived Savanna',false = name)) |>
+  mutate(name=if_else(condition = name=='Ekiti',true = 'Derived Savanna',false = name)) |>
+  mutate(name=if_else(condition = name=='Kwara',true = 'Derived Savanna',false = name)) |>
+  mutate(name=if_else(condition = name=='Nassarawa',true = 'Derived Savanna',false = name)) |>
+  mutate(name=if_else(condition = name=='Federal Capital Territory',true = 'Derived Savanna',false = name)) |>
+  mutate(name=if_else(condition = name=='Benue',true = 'Derived Savanna',false = name)) |>
+  mutate(name=if_else(condition = name=='Taraba',true = 'Derived Savanna',false = name)) |>
+  mutate(name=if_else(condition = name=='Enugu',true = 'Derived Savanna',false = name)) |>
+  mutate(name=if_else(condition = name=='Ebonyi',true = 'Derived Savanna',false = name)) |>
+  mutate(name=if_else(condition = name=='Cross River',true = 'Derived Savanna',false = name)) |>
+  mutate(name=if_else(condition = name=='Kogi',true = 'Derived Savanna',false = name)) |>
+  mutate(name=if_else(condition = name=='Ondo',true = 'Humid Forest',false = name)) |>
+  mutate(name=if_else(condition = name=='Lagos',true = 'Humid Forest',false = name)) |>
+  mutate(name=if_else(condition = name=='Edo',true = 'Humid Forest',false = name)) |>
+  mutate(name=if_else(condition = name=='Delta',true = 'Humid Forest',false = name)) |>
+  mutate(name=if_else(condition = name=='Bayelsa',true = 'Humid Forest',false = name)) |>
+  mutate(name=if_else(condition = name=='Rivers',true = 'Humid Forest',false = name)) |>
+  mutate(name=if_else(condition = name=='Abia',true = 'Humid Forest',false = name)) |>
+  mutate(name=if_else(condition = name=='Imo',true = 'Humid Forest',false = name)) |>
+  mutate(name=if_else(condition = name=='Anambra',true = 'Humid Forest',false = name)) |>
+  mutate(name=if_else(condition = name=='Akwa Ibom',true = 'Humid Forest',false = name)) |>
+  mutate(name=if_else(condition = name=='Plateau',true = 'MidAltitude',false = name)) |>
+  mutate(name=if_else(condition = name=='Niger',true = 'Southern Guinea Savanna',false = name)) |>
+  mutate(name=if_else(condition = name=='Adamawa',true = 'Southern Guinea Savanna',false = name))
+
+# Convert the name to factor
+
+dat$name <- as.factor(dat$name)
+
+
+
+
+
+
+
+
+
+
+
+# Convert the excel file to sf object
+#dat2 <- dat2 |>
+#st_as_sf(coords=c('Longitude','Latitude'),crs=4326)
+
+
+
+
+
 # Visualization
-#
+
 ggplot(data = dat,mapping = aes(x = longitude,y = latitude))+geom_sf()+geom_text(aes(label=gn_name))+
   geom_point(data = dat2,mapping = aes(x = Longitude,Latitude),size=3)+
   theme_void()
 
-
 # Updated Script
-ggplot(data = dat,mapping = aes(x = longitude,y = latitude))+geom_sf()+geom_text(aes(label=gn_name))+
-  geom_point(data = dat2,mapping = aes(x = Longitude,Latitude,col=State),size=3)+
-  theme_void()+scale_color_brewer(palette = 'Dark2',direction = -1)
-
-
+ggplot(data = dat,mapping = aes(x = longitude,y = latitude,fill=name))+geom_sf()+geom_text(aes(label=gn_name))+
+  geom_point(data = NULL,aes(x = 3.91,y = 7.51),size=3,inherit.aes = FALSE,col='red')+
+#geom_point(data = dat2,mapping = aes(x = Longitude,Latitude,col=State),size=3)+
+  theme_void()+scale_fill_brewer(name='Agro Ecological Zones',palette = 'Dark2',direction = -1)
 
 # Export the map
 
